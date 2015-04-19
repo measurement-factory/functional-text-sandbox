0ebb369a50a652a8ebd19e9aabdc081ddc15f3ed
Mark-Simulacrum <mark.simulacrum@gmail.com>
2015-04-19 15:34:40 -0600

    Add commit hashes of sandbox and functional text to gh-pages commit message.

--- deployment/deploy-sandbox.sh
+++ deployment/deploy-sandbox.sh
@@ -4,9 +4,11 @@ git checkout gh-pages
 
 git clone git@github.com:measurement-factory/functional-text-sandbox.git
 cd functional-text-sandbox
+SANDBOX_COMMIT=$(git rev-parse --verify master)
 
 git clone git@github.com:measurement-factory/functional-text.git
 cd functional-text
+PARENT_COMMIT=$(git rev-parse --verify master)
 npm install
 webpack
 cd ..
@@ -14,13 +16,12 @@ cd ..
 npm install
 webpack -p
 cp src/index.html deploy-result/index.html
-rm -fr functional-text
 cd ..
 
 cp functional-text-sandbox/deploy-result/* ./
 rm -rf functional-text-sandbox
 
 if ! git diff-index --quiet HEAD --; then
-    git commit -m "Updating gh-pages branch."
+    git commit -m "Updating gh-pages branch. (with sandbox @ $SANDBOX_COMMIT, and functional text @ $PARENT_COMMIT)"
     git push
 fi
