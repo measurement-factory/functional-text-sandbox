7a5d21bea3d3bdfb1549bcb570819ed4aef7c658
Mark-Simulacrum <mark.simulacrum@gmail.com>
2015-04-19 13:53:55 -0600

    Modify to checkout gh-pages and do all work there.

--- deployment/deploy-sandbox.sh
+++ deployment/deploy-sandbox.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
+git checkout gh-pages
+
+git clone git@github.com:measurement-factory/functional-text-sandbox.git
+cd functional-text-sandbox
+
 git clone git@github.com:measurement-factory/functional-text.git
 cd functional-text
 npm install
 webpack
 cd ..
+
 npm install
 webpack -p
 cp src/index.html deploy-result/index.html
 rm -fr functional-text
-#deploy to gh-pages
+cd ..
+
+cp functional-text-sandbox/deploy-result/* ./
+rm -rf functional-text-sandbox
